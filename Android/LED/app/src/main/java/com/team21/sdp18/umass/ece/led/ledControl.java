ae8e7e4424faf1b79411f71f1df19b2b2567d9c9
jacjacattack <jrosiel97@gmail.com>
2017-12-06 03:36:15 -0500

    App can now update graphs in real time, currently uses random numbers but should be easy to swap to bluetooth since all framework is implemented

--- Android/LED/app/src/main/java/com/team21/sdp18/umass/ece/led/ledControl.java
+++ Android/LED/app/src/main/java/com/team21/sdp18/umass/ece/led/ledControl.java
@@ -1,5 +1,7 @@
 package com.team21.sdp18.umass.ece.led;
 
+import android.app.Activity;
+import android.os.Handler;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -22,12 +24,14 @@ import android.os.AsyncTask;
 import android.widget.ToggleButton;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.Viewport;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Random;
 import java.util.UUID;
 
 import com.jjoe64.graphview.GraphView;
@@ -47,7 +51,10 @@ public class ledControl extends AppCompatActivity {
     Toast LEDinfo;
     // Generic SPP UUID for connecting to a BT serial board
     static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-    LineGraphSeries<DataPoint> series;
+
+    private static final Random RANDOM = new Random();
+    private LineGraphSeries<DataPoint> series;
+    private int lastX = 0;
 
 
     @Override
@@ -60,27 +67,41 @@ public class ledControl extends AppCompatActivity {
 
         new ConnectBT().execute(); // Call the class to connect
 
-        // Call code to connect with graphs here
         setContentView(R.layout.activity_graphs);
+        // we get graph view instance
+        GraphView graph = (GraphView) findViewById(R.id.graphx);
+        // data
+        series = new LineGraphSeries<DataPoint>();
+        graph.addSeries(series);
+        // customize a little bit viewport
+        Viewport viewport = graph.getViewport();
+        viewport.setYAxisBoundsManual(true);
+        viewport.setMinY(0);
+        viewport.setMaxY(10);
+        viewport.setScrollable(true);
 
         // Call the widgets
         toggle = (ToggleButton) findViewById(R.id.toggleButton);
 
-        //new ConnectBT().execute(); // Call the class to connect
 
         toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
                 if (isChecked) {
                     // The toggle is enabled
-                    turnOnToggle();
+                    onResume();
+
                 } else {
                     // The toggle is disabled
-                    turnOffToggle();
+                    // series.resetData();
                 }
             }
         });
 
     }
+
+
+    // OTHER METHODS
 
     private void Disconnect()
     {
@@ -97,20 +118,71 @@ public class ledControl extends AppCompatActivity {
 
     }
 
-    private void turnOffToggle()
-    {
-        //
 
 
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // we're going to simulate real time with thread that append data to the graph
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                // we add 100 new entries
+                for (int i = 0; i < 100; i++) {
+                    runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            addEntry();
+                        }
+                    });
+
+                    // sleep to slow down the add of entries
+                    try {
+                        Thread.sleep(600);
+                    } catch (InterruptedException e) {
+                        // manage error ...
+                    }
+                }
+            }
+        }).start();
     }
 
-    private void turnOnToggle()
-    {
-        //
-
-
-
+    // add random data to graph
+    private void addEntry() {
+        // here, we choose to display max 10 points on the viewport and we scroll to end
+        series.appendData(new DataPoint(lastX++, RANDOM.nextDouble() * 10d), true, 10);
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     private void rejectMessage()
     {
